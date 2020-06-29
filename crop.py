7d6a2e244e701009c5dda1cb1832d6bd66bb2ef1
zhangcheng <18811363276@163.com>
2020-06-29 15:21:26 +0800

    crop.py is committed

---
+++ crop.py
@@ -0,0 +1,23 @@
+import cv2
+import os
+from tqdm import tqdm
+
+def pic_list(path):
+    PicList = os.listdir(path)
+    M = len(PicList)
+    for i in tqdm(range(M)):
+        PicList[i] = path + PicList[i]
+        img = crop_img(PicList[i])
+        cv2.imwrite(path+"/crop/"+str(i+1)+".jpg",img)
+        pass
+
+
+def crop_img(path):
+    img = cv2.imread(path)
+    img = img[100:980,400:1780]
+    return img
+
+
+if __name__ == '__main__':
+    path = "/home/zhangcheng/data/1/"
+    PicList = pic_list(path)
